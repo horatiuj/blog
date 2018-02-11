0cc2da6363e9afa7e6c7c89ff4b741b55f8547fd
Horace <horatiu_j@yahoo.com>
2018-02-11 11:43:40 +0200

    Extended the site.

--- blog/urls.py
+++ blog/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/(?P<pk>\d+)/', views.post_detail, name='post_detail')
 
 ]
 
